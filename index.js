a768ff09b13a9c3e78a96ea485be6dd0df0b9762
Ethan Blake Castro <ethanccastro@gmail.com>
2025-03-08 12:46:43 -0800

    Added index.js and wrangler.toml

---
+++ index.js
@@ -0,0 +1,21 @@
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request));
+  });
+  
+  async function handleRequest(request) {
+    try {
+      // Fetch the index.html file
+      const html = await fetch(new URL('index.html', import.meta.url));
+      if (!html.ok) {
+          throw new Error("Could not fetch index.html");
+      }
+      const content = await html.text();
+  
+      // Return the HTML as a response
+      return new Response(content, {
+        headers: { 'Content-Type': 'text/html' },
+      });
+    } catch (error) {
+      return new Response(`Error: ${error.message}`, {status: 500})
+    }
+  }
